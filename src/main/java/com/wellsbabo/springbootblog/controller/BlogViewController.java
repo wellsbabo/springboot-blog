8957efd9923dc4039dd4e929ac303be278538443
이정관 <wellsbabo@gravity.co.kr>
2023-09-21 14:24:18 +0900

    템플릿 생성중

---
+++ src/main/java/com/wellsbabo/springbootblog/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.wellsbabo.springbootblog.controller;
+
+import com.wellsbabo.springbootblog.domain.Article;
+import com.wellsbabo.springbootblog.dto.ArticleListViewResponse;
+import com.wellsbabo.springbootblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping(value = "/articles")
+    public String getArticles(Model model){
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);   // 블로그 글 리스트 저장
+        
+        return "articleList";   // articleList.html 이라는 뷰 조회
+    }
+}
---
+++ src/main/java/com/wellsbabo/springbootblog/dto/ArticleListViewResponse.java
@@ -0,0 +1,16 @@
+package com.wellsbabo.springbootblog.dto;
+
+import com.wellsbabo.springbootblog.domain.Article;
+
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
