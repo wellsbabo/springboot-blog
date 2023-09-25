556c7f56fb3895f9c68fd97ce73555ad67bda2fe
JUNGKWAN LEE <wellsbabo@gmail.com>
2023-09-25 22:03:23 +0900

    템플릿 완성

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -9,7 +9,53 @@ if(deleteButton){
         })
             .then(() => {
                 alert('삭제 완료');
-                location.replace('/articles');
+                location.replace(`/articles`);
             });
     });
+}
+
+//수정 기능
+//id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if(modifyButton){
+    modifyButton.addEventListener('click', event => {
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+
+    fetch(`/api/articles/${id}`,{
+        method:'PUT',
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
+        })
+    })
+    .then(() => {
+        alert('수정이 완료되었습니다');
+        location.replace(`/articles/${id}`)
+    });
+  });
+}
+
+//등록 기능
+const createButton = document.getElementById('create-btn');
+if(createButton){
+    createButton.addEventListener('click', (event) => {
+        fetch(`/api/articles`,{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value,
+            }),
+        }).then(() => {
+            alert('등록 완료되었습니다.');
+            location.replace(`/articles`);
+        })
+    })
 }
